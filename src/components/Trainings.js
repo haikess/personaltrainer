bddc65c75cdac944ac1e77e5fa650cca35783145
haikess <essi.haikka@myy.haaga-helia.fi>
2021-04-21 10:26:56 +0300

    first add

---
+++ src/components/Trainings.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function Trainings() {
+    const [trainings, setTrainings] = useState ([]);
+
+    useEffect (() => {
+        fetchTrainings();
+    }, []);
+
+    const fetchTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/api/trainings')
+        .then(response => response.json())
+        .then(data => setTrainings(data.content))
+        .catch(err => console.err(err))
+    }
+
+    const columns = [
+        { field: 'date', sortable: true, filter: true },
+        { field: 'duration', sortable: true, filter: true },
+        { field: 'activity', sortable: true, filter: true },
+        { field: 'content', sortable: true, filter: true }
+    ]
+
+
+    return(
+        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto'}}> 
+            <AgGridReact
+                rowData={trainings}
+                columnDefs={columns}
+                pagination={true}
+                paginationPageSize={10}
+            />
+        </div>
+    )
+
+}
+
+export default Trainings;
