1868af8a8258a47f876771b983842152a3605563
haikess <essi.haikka@myy.haaga-helia.fi>
2021-04-28 09:06:40 +0300

    error:Module not found: Can't resolve 'material-ui/core/IconButton'

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import IconButton from 'material-ui/core/IconButton';
+import DeleteIcon from 'material-ui/icons/Delete';
 
 function Customers() {
     const [customers, setCustomers] = useState ([]);
@@ -17,14 +19,35 @@ function Customers() {
         .catch(err => console.err(err))
     }
 
+    const deleteCustomer = (url) => {
+        if (window.confirm('Are you sure?')) {
+        fetch(url, { method: 'DELETE' })
+        .then(response => {
+            if(response.ok)
+                fetchCustomers();
+            else
+                alert('Delete did not work.')
+        })
+        .catch(err => console.error(err))
+        }
+    }
+
     const columns = [
         { field: 'firstname', sortable: true, filter: true },
         { field: 'lastname', sortable: true, filter: true },
         { field: 'streetaddress', sortable: true, filter: true },
-        { field: 'postcode', sortable: true, filter: true },
-        { field: 'city', sortable: true, filter: true },
+        { field: 'postcode', sortable: true, filter: true, width: 130 },
+        { field: 'city', sortable: true, filter: true, width: 130 },
         { field: 'email', sortable: true, filter: true },
-        { field: 'phone', sortable: true, filter: true }
+        { field: 'phone', sortable: true, filter: true, width: 130 },
+        { 
+            headerName: '',
+            field: 'links.0.href',
+            cellRendererFramework: params =>
+            <IconButton color="secondary" onClick={() => deleteCustomer(params.value)}>
+                <DeleteIcon />
+            </IconButton>
+        }
     ]
 
 
@@ -35,6 +58,7 @@ function Customers() {
                 columnDefs={columns}
                 pagination={true}
                 paginationPageSize={10}
+                suppressCellSelection={true}
             />
         </div>
     )
